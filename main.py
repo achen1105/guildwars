cd3b502d0b1e1b1e216c1ff9f5072b05bceff7ee
Anita Chen <achen1105@gmail.com>
2020-12-06 19:03:23 -0800

    fixed token issue

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ Created on Sat Dec  5 14:35:25 2020
 
 @author: anitachen
 https://dev.to/p014ri5/making-and-deploying-discord-bot-with-python-4hep
+https://github.com/hugonun/discord2sheet-bot
 """
 
 import discord
@@ -12,10 +13,22 @@ from discord.ext import commands
 import os
 
 client = commands.Bot(command_prefix=".")
-#token = os.getenv("DISCORD_BOT_TOKEN")
+token = os.getenv('DISCORD_BOT_TOKEN')
 
 @client.event
 async def on_ready() :
     print("I am online")
+    
+@client.command(aliases=['addmats', 'add mats'])
+async def mats(ctx, *, materials):
+    await ctx.send(f'test added {materials}');
+    print("added mats");
 
-client.run("Nzg0OTA1OTY2ODEwMzAwNDM3.X8wGpQ.blUmtLU5ArEFUd2jEoctL4fnaQs")
+@client.command()
+async def materials(ctx, arg1: int, arg2: int):
+    #add reaction to message
+    await ctx.send(f'added mats {arg1} and {arg2} :heart:');
+    print("added mats");
+
+client.run(token);
+
