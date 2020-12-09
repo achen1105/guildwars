0823ca63bc5c5a618528e40db0a3c662cc736bf2
Anita Chen <achen1105@gmail.com>
2020-12-08 20:02:24 -0800

    tried to change the server settings and load data from spreadsheet

--- mainfeatures.py
+++ mainfeatures.py
@@ -6,6 +6,7 @@ Created on Sat Dec  5 14:35:25 2020
 @author: anitachen
 https://dev.to/p014ri5/making-and-deploying-discord-bot-with-python-4hep
 https://github.com/hugonun/discord2sheet-bot
+mainfeatures.py doesn't work, only main.py does currently'
 """
 
 # BSD 3-Clause License
@@ -15,17 +16,72 @@ https://github.com/hugonun/discord2sheet-bot
 import discord
 from discord.ext import commands
 import os
+import json
 from gsheet import *
 
 client = discord.Client()
 sheet = gsheet()
 #default test sheet
-SPREADSHEET_ID = '1FMndMT4e6RX1SEzgwLuDDv-ANN75WxbnLAa0CW5JQvk';
-RANGE_NAME = 'A1';
-FIELDS = 2;
+SPREADSHEET_ID = '1-BsssPXufgAH2-n5ygUW662yAjHdvds8dzAfayifoc4';
+RANGE_NAME = '\'member sheet\'!A3:A40';
+RANGE_SAND = '\'member sheet\'!G3:G40';
+RANGE_CRYSTAL = '\'member sheet\'!H3:H40';
+FIELDS = 3;
 token = os.getenv('DISCORD_BOT_TOKEN')
 
+def getSettings(client, message):
+    with open('settings.json', 'r') as f:
+        settings = json.load(f)
+        
+    return settings[str(message.guild.id)];
 
+bot = commands.Bot(command_prefix=getSettings)
+
+@bot.event
+async def on_guild_join(guild):
+    with open('settings.json', 'r') as f:
+        settings = json.load(f)
+        
+    settings[str(guild.id)] = ".";
+    
+    #write to json file
+    with open('settings.json', 'w') as f:
+        json.dump(settings, f, indent=4);    
+
+@bot.event
+async def on_guild_remove(guild):
+    with open('settings.json', 'r') as f:
+        settings = json.load(f)
+        
+    settings.pop(str(guild.id));
+    
+    #write to json file
+    with open('settings.json', 'w') as f:
+        json.dump(settings, f, indent=4);    
+        
+@bot.command()
+async def changeprefix(ctx, prefix):
+    with open('settings.json', 'r') as f:
+        settings = json.load(f)
+        
+    settings[str(ctx.guild.id)] = prefix;
+    
+    #write to json file
+    with open('settings.json', 'w') as f:
+        json.dump(settings, f, indent=4);  
+        
+    
+@bot.command(aliases=['mats', 'addmats'])
+async def materials(ctx, arg1: int, arg2: int):
+    #add reaction to message
+    await ctx.send(f'added mats {arg1} and {arg2} :heart:');
+    print("added mats");
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'hello!! :heart:');
+    print("said hello");
+        
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
@@ -35,6 +91,7 @@ async def on_message(message):
    # uses global variables to store
     global SPREADSHEET_ID
     global RANGE_NAME
+    global RANGE_MATS
     global FIELDS
     
     if message.author == client.user:
@@ -54,11 +111,15 @@ async def on_message(message):
         await message.channel.send(f'Your sheet ID has been successfully changed to {msg2}!')
         print(SPREADSHEET_ID);
         
+    # change range name
+    if message.content.startswith('!rangename'):
+        msg4 = message.content[11:];
+        RANGE_NAME = msg4;
+        await message.channel.send(f'Your rangename has been successfully changed to {msg4}!')
+        print(RANGE_NAME);
+        
     # Command to insert data to excel
     if message.content.startswith('!s '):
-        #SPREADSHEET_ID = '1FMndMT4e6RX1SEzgwLuDDv-ANN75WxbnLAa0CW5JQvk' # Add ID here
-        #RANGE_NAME = 'A1'
-        #FIELDS = 2 # Amount of fields/cells
 
         # Code
         msg = message.content[3:]
@@ -66,8 +127,19 @@ async def on_message(message):
         if len(result) == FIELDS:
             # Add
             print(message.created_at)
-            DATA = [message.author.name] + [str(message.author.id)] + [str(message.created_at)] + result
-            sheet.add(SPREADSHEET_ID, RANGE_NAME, DATA)
+            #name = [message.author.name] + [str(message.created_at)]
+            #sheet.add(SPREADSHEET_ID, RANGE_NAME, name)
+            
+            nameValues = service.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME).execute()
+            rows = result.get('values', [])
+            print('{0} rows retrieved.'.format(len(rows)))
+            
+            ign = len[0]
+            for x in nameValues:
+                if ign == x:
+                    await message.channel.send('Your data has been successfully submitted!')
+            
+            sheet.add(SPREADSHEET_ID, RANGE_MATS, result)
             await message.channel.send('Your data has been successfully submitted!')
         else:
             # Needs more/less fields
@@ -80,6 +152,20 @@ async def on_message(message):
             if muser.id == client.user.id:
                 if any(word in message.content for word in ['whois','who is','Help','help','info']):
                     await message.channel.send('This bot was made by hugonun(https://github.com/hugonun/).\nSource code: https://github.com/hugonun/discord2sheet-bot\nEdited by Jia#4778 https://github.com/achen1105')
+    
+    # Non sheet related
+    # Color roles
+    if message.content.startswith('!changecolor'):
+        msg3 = message.content[13:];
+        hexcode = '0x'+msg3;
+        await message.guild.create_role(name=message.author.name, color=discord.Color(hexcode));
+        role = discord.utils.get(message.guild.roles, name=message.author.name)
+        await role.edit(position(1));
+        await message.author.add_roles(role);
+    
+    # Fun responses
+    if message.content.find("hello momo") != -1:
+        await message.channel.send(f'Hello {message.author.mention}! :two_hearts:');
 
 
 client.run(token); # Add bot token here
